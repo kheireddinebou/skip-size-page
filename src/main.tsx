a7bccfc928d2b45053a1f0db19a08923f0bdaef2
Kheireddine Boukhatem <105202227+kheireddinebou@users.noreply.github.com>
2025-06-08 17:16:21 +0100

    refactor: remove SkipSizeSelector component and update main render to use App

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
 import "./styles/index.css";
-import SkipSizeSelector from "./SkipSizeSelector.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <SkipSizeSelector />
+    <App />
   </StrictMode>
 );
